1c2383957d6ddafe327715d20e73d43a694b1565
Valentino Nathan <nathan070907@gmail.com>
2026-05-06 12:11:44 +0800

    react-routing init

--- CinemaONE/src/main.jsx
+++ CinemaONE/src/main.jsx
@@ -1,7 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+
+const router = createBrowserRouter([
+  {path: "/", element: <App />},
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
